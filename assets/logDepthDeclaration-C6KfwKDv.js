d4513f12257339a9d45d8daed1ac3d5b72f79080
Vicente Cartas <vicente.cartas@outlook.com>
2025-07-17 13:34:15 -0700

    Adding babylon code

---
+++ assets/logDepthDeclaration-C6KfwKDv.js
@@ -0,0 +1,4 @@
+import{S as e}from"./index-spQMz7gd.js";const t="logDepthDeclaration",a=`#ifdef LOGARITHMICDEPTH
+uniform float logarithmicDepthConstant;varying float vFragmentDepth;
+#endif
+`;e.IncludesShadersStore[t]||(e.IncludesShadersStore[t]=a);
