15c51bdb21bbc565b4dc2d8da0054fd751b4e8b1
Vicente Cartas <vicente.cartas@outlook.com>
2025-07-23 10:26:14 -0700

    Adding different optimizations versions

---
+++ assets/sprites.fragment-e_GQGd6r.js
@@ -0,0 +1,51 @@
+import{S as e}from"./worker-BR_GWEAC.js";import"./logDepthDeclaration-BPtDQ4qb.js";const o="fogFragmentDeclaration",s=`#ifdef FOG
+#define FOGMODE_NONE 0.
+#define FOGMODE_EXP 1.
+#define FOGMODE_EXP2 2.
+#define FOGMODE_LINEAR 3.
+const E=2.71828;uniform vFogInfos: vec4f;uniform vFogColor: vec3f;varying vFogDistance: vec3f;fn CalcFogFactor()->f32
+{var fogCoeff: f32=1.0;var fogStart: f32=uniforms.vFogInfos.y;var fogEnd: f32=uniforms.vFogInfos.z;var fogDensity: f32=uniforms.vFogInfos.w;var fogDistance: f32=length(fragmentInputs.vFogDistance);if (FOGMODE_LINEAR==uniforms.vFogInfos.x)
+{fogCoeff=(fogEnd-fogDistance)/(fogEnd-fogStart);}
+else if (FOGMODE_EXP==uniforms.vFogInfos.x)
+{fogCoeff=1.0/pow(E,fogDistance*fogDensity);}
+else if (FOGMODE_EXP2==uniforms.vFogInfos.x)
+{fogCoeff=1.0/pow(E,fogDistance*fogDistance*fogDensity*fogDensity);}
+return clamp(fogCoeff,0.0,1.0);}
+#endif
+`;e.IncludesShadersStoreWGSL[o]||(e.IncludesShadersStoreWGSL[o]=s);const r="logDepthFragment",i=`#ifdef LOGARITHMICDEPTH
+fragmentOutputs.fragDepth=log2(fragmentInputs.vFragmentDepth)*uniforms.logarithmicDepthConstant*0.5;
+#endif
+`;e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=i);const n="fogFragment",c=`#ifdef FOG
+var fog: f32=CalcFogFactor();
+#ifdef PBR
+fog=toLinearSpace(fog);
+#endif
+color= vec4f(mix(uniforms.vFogColor,color.rgb,fog),color.a);
+#endif
+`;e.IncludesShadersStoreWGSL[n]||(e.IncludesShadersStoreWGSL[n]=c);const t="imageProcessingCompatibility",g=`#ifdef IMAGEPROCESSINGPOSTPROCESS
+fragmentOutputs.color=vec4f(pow(fragmentOutputs.color.rgb, vec3f(2.2)),fragmentOutputs.color.a);
+#endif
+`;e.IncludesShadersStoreWGSL[t]||(e.IncludesShadersStoreWGSL[t]=g);const f="spritesPixelShader",a=`uniform alphaTest: i32;varying vColor: vec4f;varying vUV: vec2f;var diffuseSamplerSampler: sampler;var diffuseSampler: texture_2d<f32>;
+#include<fogFragmentDeclaration>
+#include<logDepthDeclaration>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+#ifdef PIXEL_PERFECT
+fn uvPixelPerfect(uv: vec2f)->vec2f {var res: vec2f= vec2f(textureDimensions(diffuseSampler,0));var uvTemp=uv*res;var seam: vec2f=floor(uvTemp+0.5);uvTemp=seam+clamp((uvTemp-seam)/fwidth(uvTemp),vec2f(-0.5),vec2f(0.5));return uvTemp/res;}
+#endif
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#ifdef PIXEL_PERFECT
+var uv: vec2f=uvPixelPerfect(input.vUV);
+#else
+var uv: vec2f=input.vUV;
+#endif
+var color: vec4f=textureSample(diffuseSampler,diffuseSamplerSampler,uv);var fAlphaTest: f32= f32(uniforms.alphaTest);if (fAlphaTest != 0.)
+{if (color.a<0.95) {discard;}}
+color*=input.vColor;
+#include<logDepthFragment>
+#include<fogFragment>
+fragmentOutputs.color=color;
+#include<imageProcessingCompatibility>
+#define CUSTOM_FRAGMENT_MAIN_END
+}`;e.ShadersStoreWGSL[f]||(e.ShadersStoreWGSL[f]=a);const m={name:f,shader:a};export{m as spritesPixelShaderWGSL};
